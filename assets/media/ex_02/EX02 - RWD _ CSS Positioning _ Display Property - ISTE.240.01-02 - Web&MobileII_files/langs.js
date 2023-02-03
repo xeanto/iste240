43b5ab06a4bb4dc5202297eb697e1c0bb8b5f691
Casey Malley <casey@xeanto.us>
2023-02-03 11:58:55 -0500

    Adding relevant files

---
+++ assets/media/ex_02/EX02 - RWD _ CSS Positioning _ Display Property - ISTE.240.01-02 - Web&MobileII_files/langs.js
@@ -0,0 +1,3 @@
+// auto-generated
+if (!window.D2L) window.D2L = {};
+if (!window.D2L.tinymceLangs) window.D2L.tinymceLangs = ['ar', 'bg_BG', 'ca', 'cs', 'da', 'de', 'el', 'es', 'eu', 'fa', 'fi', 'fr_FR', 'he_IL', 'hi', 'hr', 'hu_HU', 'id', 'it', 'ja', 'kk', 'ko_KR', 'ms', 'nb_NO', 'nl', 'pl', 'pt_BR', 'pt_PT', 'ro', 'ru', 'sk', 'sl_SI', 'sv_SE', 'th_TH', 'tr', 'uk', 'vi', 'zh_CN', 'zh_TW'];
