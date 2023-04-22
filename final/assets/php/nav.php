56850446b38b319d337ccd55065e87a93b7b4295
Casey Malley <casey@xeanto.us>
2023-04-22 16:38:10 -0400

    Remove commented out css and change hardcoding to variables in header pph

--- final/assets/php/nav.php
+++ final/assets/php/nav.php
@@ -4,19 +4,21 @@
     </div>
     <?php
     $blacklist = array('.', '..');
+    // title variable assignment regex
     $pattern = '/\$title\s*=\s*[\'"](.+?)[\'"]/';
-    foreach (glob('/home/MAIN/cam8940/Sites/iste240/final/content/*', GLOB_ONLYDIR) as $dir) {
+    // loop through each directory in the content folder
+    foreach (glob($root . 'content/*', GLOB_ONLYDIR) as $dir) {
         $dir = basename($dir);
         // open divs and set name of nav-item
         echo '<div class="nav-item">
             <a class="nav-button">' . ucfirst(strtolower($dir)) . '</a>
             <div class="nav-content">';
-        // get all directories in $dir
-        foreach (glob('/home/MAIN/cam8940/Sites/iste240/final/content/' . $dir . '/*', GLOB_ONLYDIR) as $subdir) {
+        // loop through all directories in $dir
+        foreach (glob($root . 'content/' . $dir . '/*', GLOB_ONLYDIR) as $subdir) {
             $subdir = basename($subdir);
+            // get contents of index.phps
+            $contents = file_get_contents($root . 'content/' . $dir . '/' . $subdir . '/index.php');
             // get title from index.php
-            $contents = file_get_contents('/home/MAIN/cam8940/Sites/iste240/final/content/' . $dir . '/' . $subdir . '/index.php');
-            $pattern = '/\$title\s*=\s*[\'"](.+?)[\'"]/';
             if (preg_match($pattern, $contents, $matches)) {
                 $pagetitle = $matches[1];
                 echo '<a href="' . $webroot . 'content/' . $dir . '/' . $subdir . '">' . $pagetitle . '</a>';
