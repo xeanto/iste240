b7799a12b3799b7a104c6b732ab3b22fde24386e
Casey Malley <casey@xeanto.us>
2023-04-22 15:01:30 -0400

    adding dynamic nav population

--- final/assets/php/nav-new.php
+++ final/assets/php/nav-new.php
@@ -12,13 +12,51 @@
             <a href="<?php echo $root ?>pages/pgh_bridges.html">Our Bridges</a>
         </div>
     </div>
-    <div class="nav-item">
-        <?php
-        $directory = opendir('/home/MAIN/cam8940/Sites/iste240/final/pages');
-        while (false !== ($file = readdir($directory))) {
-            echo $file;
+
+    <?php
+    $blacklist = array('.', '..');
+    $pattern = '/\$title\s*=\s*[\'"](.+?)[\'"]/';
+    foreach (glob('/home/MAIN/cam8940/Sites/iste240/final/content/*', GLOB_ONLYDIR) as $dir) {
+        $dir = basename($dir);
+        // open divs and set name of nav-item
+        echo '<div class="nav-item">
+            <a class="nav-button">' . $dir . '</a>
+            <div class="nav-content">';
+        // get all directories in $dir
+        foreach (glob('/home/MAIN/cam8940/Sites/iste240/final/content/' . $dir . '/*', GLOB_ONLYDIR) as $subdir) {
+            $subdir = basename($subdir);
+            // get title from index.php
+            $contents = file_get_contents('/home/MAIN/cam8940/Sites/iste240/final/content/' . $dir . '/' . $subdir . '/index.php');
+            $pattern = '/\$title\s*=\s*[\'"](.+?)[\'"]/';
+            if (preg_match($pattern, $contents, $matches)) {
+                $pagetitle = $matches[1];
+                echo '<a href="content/' . $dir . '/' . $subdir . '">' . $pagetitle . '</a>';
+            }
         }
-        closedir($directory);
-        ?>
-    </div>
+        // close divs
+        echo '</div>
+            </div>';
+    }
+    ?>
+
+    <!-- <div class="nav-item">
+        <a class="nav-button">Other</a>
+        <div class="nav-content">
+            
+            // $filedir = '/home/MAIN/cam8940/Sites/iste240/final/content/other';
+            // $directory = opendir($filedir);
+            // $blacklist = array('.', '..');
+            // $pattern = '/\$title\s*=\s*[\'"](.+?)[\'"]/';
+            // while (false !== ($file = readdir($directory))) {
+            //     if (!in_array($file, $blacklist)) {
+            //         $contents = file_get_contents($filedir . "/" . $file . '/index.php');
+            //         if (preg_match($pattern, $contents, $matches)) {
+            //             $pagetitle = $matches[1];
+            //             echo '<a href="content/' . $file . '">' . $pagetitle . '</a>';
+            //         }
+            //     }
+            // }
+            // closedir($directory);
+        </div>
+    </div> -->
 </nav>
